f3411705c5b673f1b19f9210d60e856ac4b1ee9e
Willyang10x <willyangabrielfxt@gmail.com>
2024-12-02 19:30:04 -0300

    Atualização .env

--- configBD.js
+++ configBD.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'Acaiteria'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
